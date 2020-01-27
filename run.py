58c2d1d289c389fe76d04c28ca58d8ac9b04866c
Bryan Bar <bryanbarajas96@gmail.com>
2020-01-27 12:41:51 -0800

    added p5 and bottom navbar

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from frontend import app
 
 if __name__ == '__main__':
-    app.run(debug=True) # debugging should be set to false at deployment
+    app.run(debug=True)  # debugging should be set to false at deployment
