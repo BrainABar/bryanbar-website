378a68c019272daace84fcf4e1bfe3a964e20f53
Bryan Bar <bryanbarajas96@gmail.com>
2020-01-27 17:59:26 -0800

    added orbit as background

---
+++ frontend/static/js/dotObject.js
@@ -0,0 +1,23 @@
+class Dot{
+    constructor(radius, distance, orbitSpeed, color) {
+        this.radius = radius;
+        this.angle = random(TWO_PI);
+        this.distance = distance;
+        this.orbitSpeed = orbitSpeed;
+        this.color = color;
+    }
+
+    show(){
+        push();
+        fill(this.color);
+        rotate(this.angle);
+        translate(this.distance, 0);
+        ellipse(0,0,this.radius);
+        pop();
+    }
+
+    orbit(){
+        this.angle += this.orbitSpeed;
+    }
+}
+
--- frontend/static/js/sketch.js
+++ frontend/static/js/sketch.js
@@ -1,14 +1,29 @@
-var canvas;
-var circles = []
+
+let canvas;
+let amount = 50;
+var screenobjs = []; // screen objects
 
 function setup(){
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0,0);
-    canvas.style('z-index', '-1')
+    canvas.style('z-index','-1');
+    var edge = 30; // last position of a planet
+    for(let i=0;i<4;i++){
+        edge = edge + random(30,60);
+        let planetColor = color(255);
+        let planetObj = new Dot(random(2,30), edge, abs((i+1)-4)/200, planetColor);
+        screenobjs.push(planetObj);
+    }
 }
 
 function draw(){
     background(0);
-    fill(frameCount%255, frameCount/2%255, frameCount%255);
-    circle(frameCount%windowWidth,frameCount%windowHeight, 10);
+    fill(255);
+    translate(width/2, height/2)
+    ellipse(0,0,30,30);
+    for(let i=0;i<screenobjs.length;i++){
+        screenobjs[i].show();
+        screenobjs[i].orbit();
+    }
+
 }
